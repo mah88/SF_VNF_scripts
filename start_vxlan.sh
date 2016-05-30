4f23384d91462fc30dbdac970aa22b5efdb4a586
mah88 <a.medhat.h@gmail.com>
2016-05-30 18:44:30 +0200

    Update start_vxlan.sh

--- start_vxlan.sh
+++ start_vxlan.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-sudo python ~/home/ubuntu/vxlan_tool.py -i eth0 -d forward -v on
+git clone https://github.com/anfredette/vxlan_tool.git
+sudo python ~/vxlan_tool/vxlan_tool.py -i eth0 -d forward -v on
