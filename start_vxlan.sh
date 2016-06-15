dd96781b420f02272ef507cc75ead13889676ebc
mah88 <a.medhat.h@gmail.com>
2016-06-15 11:23:47 +0200

    Update start_vxlan.sh

--- start_vxlan.sh
+++ start_vxlan.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git clone https://github.com/mah88/nsh_tool.git
-screen -d -m -S VXLAN_Tool python ~/nsh_tool/nsh_tools/vxlan_tool.py -i eth0 -d forward -v on
+screen -d -m -S vxlan_tool python ~/nsh_tool/nsh_tools/vxlan_tool.py -i eth0 -d forward -v on
