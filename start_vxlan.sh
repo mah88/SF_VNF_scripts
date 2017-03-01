a47b0d0223d6d18ccf3706ba8cd145ab34bf65aa
mah88 <a.medhat.h@gmail.com>
2017-03-01 13:33:05 +0100

    Update start_vxlan.sh

--- start_vxlan.sh
+++ start_vxlan.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-#sudo screen -d -m -S Vxlan python /etc/nsh_tool/nsh_tools/vxlan_tool_new.py -i ens3 -d forward -v on -b 22
-sudo python /etc/nsh_tool/nsh_tools/vxlan_tool_new.py -i ens3 -d forward -v on
+sudo screen -d -m -S Vxlan python /etc/nsh_tool/nsh_tools/vxlan_tool_new.py -i ens3 -d forward -v on 
