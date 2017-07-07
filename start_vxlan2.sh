3ee6fd84f04d0532d54f1c0795d827dce81e8fe9
mah88 <a.medhat.h@gmail.com>
2017-07-07 14:17:29 +0200

    Update start_vxlan2.sh

--- start_vxlan2.sh
+++ start_vxlan2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #screen -d -m -S Vxlan 
-python /nsh_tool/nsh_tools/vxlan_tool.py -i eth0 -d forward -v off -pc x
+python /nsh_tool/nsh_tools/vxlan_tool.py -i eth0 -d forward -v off -bd 12.0.0.3 -bs 12.0.0.4
