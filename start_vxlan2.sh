daa61030b65ba3c2f399e8d8601ffc8931784338
mah88 <a.medhat.h@gmail.com>
2018-05-02 13:29:41 +0200

    Update start_vxlan2.sh

--- start_vxlan2.sh
+++ start_vxlan2.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-screen -d -m -S Vxlan python /etc/nsh_tool/nsh_tools/vxlan_tool_new_mod.py -i eth0 -d forward -v off -bd 12.0.0.3 -bs 12.0.0.4
+screen -d -m -S Vxlan python /etc/nsh_tool/nsh_tools/vxlan_tool_new.py -i eth0 -d forward -v on
